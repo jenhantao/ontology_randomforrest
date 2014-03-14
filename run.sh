7e0e0119f427dc1dccc554e4cbf2c40e2038e3bb
Jenhan Tao <jenhantao@gmail.com>
2014-03-14 11:52:22 -0700

    added scripts for computing dtance

---
+++ run.sh
@@ -0,0 +1,4 @@
+python rfDistanceCounter.py costanzo/trees/tree_1/ GO_biological_process.term_term.txt 
+python rfDistanceCounter.py costanzo/trees/tree_2/ GO_biological_process.term_term.txt 
+python rfDistanceCounter.py costanzo/trees/tree_3/ GO_biological_process.term_term.txt 
+python rfDistanceCounter.py costanzo/trees/tree_4/ GO_biological_process.term_term.txt 
