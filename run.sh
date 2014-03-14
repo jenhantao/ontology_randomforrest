cda1daf375e70d2e068b5c0f7fadeb8cb0c42529
Jenhan Tao <jenhantao@gmail.com>
2014-03-14 12:56:21 -0700

    refactored run script for cluster

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
-python rfDistanceCounter.py costanzo/trees/tree_1/ GO_biological_process.term_term.txt 
-python rfDistanceCounter.py costanzo/trees/tree_2/ GO_biological_process.term_term.txt 
-python rfDistanceCounter.py costanzo/trees/tree_3/ GO_biological_process.term_term.txt 
-python rfDistanceCounter.py costanzo/trees/tree_4/ GO_biological_process.term_term.txt 
+treeFiles=()
+for treeFileSplit in $1*txt
+do
+treeFiles+=($treeFileSplit)
+done
+
+# loop over tree files and compute distances
+for treeFile in ${treeFiles[@]}
+do
+pythonOut=${treeFile/.txt/_distance.npy}
+if [ ! -f $pythonOut ]
+then
+echo python treeDistanceCounter.py $treeFile $2
+# submit jobs to cluster
+qsub python treeDistanceCounter.py $treeFile
+fi
+done
+
+# call script to sum individual distances and compute final matrix
+
